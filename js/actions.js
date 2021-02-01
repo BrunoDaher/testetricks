860b1599ff602c495b245cefd900aca2fd94dc08
BrunoDaher <brunus9@gmail.com>
2021-01-31 22:35:12 -0300

    commit correção de menu

--- js/actions.js
+++ js/actions.js
@@ -1,12 +1,29 @@
 
-function init(){
+const menu = document.querySelectorAll('.headMenu');
 
+function menuInit(){
     let menu = document.querySelectorAll('.headMenu');
     menu.forEach(item => handl(item));
 }
 
 function handl(item){
     item.addEventListener('click', event => {        
+        release();
+        item.parentNode.classList.add('active');        
+        document.title = item.innerText;        
         getFrag(item.target,'main');
       })
+}
+
+let sheet = document.styleSheets[0];
+    let rules = sheet.cssRules || sheet.rules;
+    
+    console.log(rules);
+    
+
+function release(){
+    
+
+    let menu = document.querySelectorAll('.headMenu');
+        menu.forEach(item => item.parentNode.classList.remove('active'));
 }
--- js/builder.js
+++ js/builder.js
@@ -6,18 +6,20 @@ function getFrag(url,divName){
 
     fetch(path + url).then(function (response) {
         // The API call was successful!
+        console.log(response);
         return response.text();
     }).then(function (html) {
         // This is the HTML from our response as a text string   
         let _divTarget = document.querySelector(divName);
         _divTarget.innerHTML = html; 
-        url=='menu.html' ? init():"";
+        url=='menu.html' ? menuInit():"";
     }).catch(function (err) {
         // There was an error
         //console.warn('Something went wrong.', err);
     });
     
 }
+
 
 
 getFrag('menu.html', 'header');
